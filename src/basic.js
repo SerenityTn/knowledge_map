07d521e337595259e47d3223013b57bfff7e1739
Serenity <serenity1994@hotmail.fr>
2017-03-28 08:36:42 +0100

    cleaning code + intergrating trello api

--- src/basic.js
+++ src/basic.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import events from './events';
 import moment from 'moment';
+import Trello from 'trello';
 
-BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-let Basic = React.createClass({
-  render(){
-    return (
-      <BigCalendar
-        {...this.props}
-        events={events}
-        defaultDate={new Date(2015, 3, 1)}
-      />
-    )
-  }
-})
+BigCalendar.momentLocalizer(moment);
 
-export default Basic;
+class Calendar extends Component {
+	constructor(){
+		super();
+		const appKey = "b707659cd995ea808667eadfe7580cc6";
+		const token = "23a6b51ce2d02f622d889695005f37d4bce0e8b23c2463cff1a1279b48a4d3c4";
+		let trello = new Trello(appKey, token);
+		trello.makeRequest('get', '/1/members/me', { webhooks: true })
+		.then((res) => {
+			console.log(res)
+		});
+	}
+	render(){
+		return (
+			<BigCalendar
+			{...this.props}
+			events={events}
+			defaultDate={new Date(2015, 3, 1)}
+			/>
+			)
+	}
+}
+
+export default Calendar;
