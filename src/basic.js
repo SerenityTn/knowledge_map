f82323721b066c527a3702a33828f86e3fb3267c
Serenity <serenity1994@hotmail.fr>
2017-03-30 07:59:40 +0100

    changing trello intergation

--- src/basic.js
+++ src/basic.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
-import events from './events';
 import moment from 'moment';
 import Trello from 'trello';
 
@@ -9,22 +8,48 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 BigCalendar.momentLocalizer(moment);
 
 class Calendar extends Component {
+	static get APPKEY (){
+		return "b707659cd995ea808667eadfe7580cc6";
+	}
+	static get TOKEN(){
+		return "23a6b51ce2d02f622d889695005f37d4bce0e8b23c2463cff1a1279b48a4d3c4"
+	}	
+
+	static trello = new Trello(Calendar.APPKEY, Calendar.TOKEN);
+	
 	constructor(){
 		super();
-		const appKey = "b707659cd995ea808667eadfe7580cc6";
-		const token = "23a6b51ce2d02f622d889695005f37d4bce0e8b23c2463cff1a1279b48a4d3c4";
-		let trello = new Trello(appKey, token);
-		trello.makeRequest('get', '/1/members/me', { webhooks: true })
+		this.state = {
+			events: this.getEvents()
+		}						
+		Calendar.trello.makeRequest('get', '/1/members/me/boards', { webhooks: true })
 		.then((res) => {
 			console.log(res)
 		});
 	}
-	render(){
+
+	getEvents(){
+		return [  
+		{
+			'title': 'Serenity Event',
+			'start': new Date(2017, 3, 30),
+			'end': new Date(2017, 3, 30)
+		},
+
+		{
+			'title': 'DTS STARTS',
+			'start': new Date(2017, 3, 31, 12, 0, 0),
+			'end': new Date(2017, 3, 31, 13, 0, 0)
+		}
+		]
+	}	
+
+	render(){		
 		return (
 			<BigCalendar
 			{...this.props}
-			events={events}
-			defaultDate={new Date(2015, 3, 1)}
+			events = { this.getEvents()}
+			defaultDate={new Date(2017, 3, 30)}
 			/>
 			)
 	}
